aad518ed19055fac21ca2583f14aa1eb7eaff5d5
Nigar <95375286+MNIGARR@users.noreply.github.com>
2026-04-02 00:11:30 +0400

    Refactor ProjectSection to add new project link
    Updated ProjectSection component to include a new project link and removed commented-out code.

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -3,26 +3,25 @@ export const ProjectSection = () => {
         <section id="projects">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
                 <div className="gradient-border p-6 card-hover rounded-3xl gap-4 flex items-start">
-                     <img>
+                    <img>
                     </img>
                     <a href="https://html-css-project-henna-iota.vercel.app/">Visit site</a>
-
-
                 </div>
                 <div className="gradient-border p-6 card-hover rounded-3xl gap-4 flex items-start">
-                     <img>
+                    <img>
                     </img>
                     <a href="https://snake-game-five-coral-14.vercel.app/">Visit site</a>
                 </div>
-                <div className="gradient-border p-6 card-hover rounded-3xl gap-4 flex items-start">
-                     <img>
+                {/* <div className="gradient-border p-6 card-hover rounded-3xl gap-4 flex items-start">
+                    <img>
                     </img>
                     <a href="https://html-css-project-henna-iota.vercel.app/">Visit site</a>
-
-
+                </div> */}
+                 <div className="gradient-border p-6 card-hover rounded-3xl gap-4 flex items-start">
+                     <img>
+                    </img>
+                    <a href="https://manolya-handmade.vercel.app/">Visit site</a>
                 </div>
-                
-               
             </div>
 
             <div>
